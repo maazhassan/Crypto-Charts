1087b3c76cdc2e7f50455b6c410bdb03ff216c51
maaz.hassan <h.maaz809@hotmail.com>
2020-11-23 02:24:27 -0700

    Setting up

--- key.py
+++
@@ -1,1 +0,0 @@
-key = '61ovCgYqVQk8sazCwuGG'
--- test.py
+++
@@ -1,8 +0,0 @@
-import base64
-encrypted="FkYSDwtXXB4SRlpSFB4KEwQbHBMVTUYCFQRYXAwGFB9PFANNRgQJHFFcAAQFXUQUHggHBxUaQEpK QVtaT11XDhMEHgFWVQhGTVpPVVoFCAQMDVlcAxVGWlIUHhgPDRULX1wJRk1aT0ZYDwMIDhsTGVdB RgkJUlxKTUFdDltWSkFbWk9DUANARgc="
-my_eyes=str.encode("maazh49")
-decoded=base64.b64decode(encrypted)
-decrypted=""
-for i in range(0,len(decoded)):
-    decrypted+=chr((my_eyes[i%len(my_eyes)] ^ decoded[i]))
-print(decrypted)
